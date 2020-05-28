bc189483485b4cf32b7aab15aa4d954e37ff3a6d
zyan-dev <zyan.dev101@gmail.com>
2020-05-27 21:04:32 -0600

    working on name pronunciation screen for verification flow

--- src/containers/Auth/index.js
+++ src/containers/Auth/index.js
@@ -3,6 +3,7 @@ import {createStackNavigator} from '@react-navigation/stack';
 import SendSMSScreen from './SendSMS';
 import VerifySMSScreen from './VerifySMS';
 import CompleteSignUp from './CompleteSignUp';
+import NamePronunciationScreen from './NamePronunciation';
 import OurValuesScreen from './OurValues';
 import OurCommunityRuleScreen from './OurCommunityRules';
 import WelcomeScreen from './Welcome';
@@ -12,10 +13,16 @@ const Stack = createStackNavigator();
 class VerificationStack extends React.Component {
   render() {
     return (
-      <Stack.Navigator initialRouteName="Auth_SendSMS" headerMode="none">
+      <Stack.Navigator
+        initialRouteName="Auth_NamePronunciation"
+        headerMode="none">
         <Stack.Screen name="Auth_SendSMS" component={SendSMSScreen} />
         <Stack.Screen name="Auth_VerifySMS" component={VerifySMSScreen} />
         <Stack.Screen name="Auth_CompleteSignUp" component={CompleteSignUp} />
+        <Stack.Screen
+          name="Auth_NamePronunciation"
+          component={NamePronunciationScreen}
+        />
         <Stack.Screen name="Auth_OurValues" component={OurValuesScreen} />
         <Stack.Screen name="Auth_Welcome" component={WelcomeScreen} />
         <Stack.Screen
