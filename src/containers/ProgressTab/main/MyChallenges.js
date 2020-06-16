eca5cc10ccf057c5f1726d3226c0fef63601ec93
johna525 <john.amuesi525@gmail.com>
2020-06-16 21:56:19 +0100

    Update my challenge screen

--- src/containers/ProgressTab/main/MyChallenges.js
+++ src/containers/ProgressTab/main/MyChallenges.js
@@ -3,6 +3,7 @@ import {Alert, FlatList} from 'react-native';
 import {connect} from 'react-redux';
 import {withTranslation} from 'react-i18next';
 import * as _ from 'lodash';
+import Carousel from 'react-native-snap-carousel';
 import {challengeActions, postActions} from 'Redux/actions';
 import {
   MCRootView,
@@ -27,6 +28,16 @@ class MyChallenges extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+  }
+
+  componentDidMount() {
+    const {myChallenges, focusedChallenge} = this.props;
+    const findIndex = myChallenges.findIndex(
+      i => i._id === focusedChallenge._id,
+    );
+    setTimeout(() => {
+      this._carousel && this._carousel.snapToItem(findIndex, true);
+    }, 2000);
   }
 
   onPressChallenge = item => {
@@ -99,41 +110,28 @@ class MyChallenges extends React.Component {
     if (!focusedChallenge) return null;
     const selected = focusedChallenge._id === item._id;
     return (
-      <MCButton
-        mr={20}
-        pt={1}
-        pb={1}
-        pl={1}
-        pr={1}
-        br={10}
-        onPress={() => this.onPressChallenge(item)}
-        style={{
-          borderWidth: 2,
-          borderColor: selected ? theme.colors.outline : 'transparent',
-        }}>
-        <NativeCard width={250} justify="flex-start" pv={1}>
-          <H3 weight="bold">{item.title}</H3>
-          <MCView row>
-            <MCView width={150} align="center">
-              <MCView row wrap mb={10}>
-                {combineChallenges(item.challenges)
-                  .slice(0, 4)
-                  .map(i => {
-                    return getChallengeIcon(i.category, theme.colors.text);
-                  })}
-              </MCView>
-              <DividerLine width={120} />
-              <H4 mt={10}>
-                {item.skills.length} {t('outline_skills')}
-              </H4>
+      <NativeCard width={250} justify="flex-start" pv={1}>
+        <H3 weight="bold">{item.title}</H3>
+        <MCView row>
+          <MCView width={150} align="center">
+            <MCView row wrap mb={10}>
+              {combineChallenges(item.challenges)
+                .slice(0, 4)
+                .map(i => {
+                  return getChallengeIcon(i.category, theme.colors.text);
+                })}
             </MCView>
-            <MCView width={100} align="center">
-              <H4>{t('label_days_left')}</H4>
-              <H1 weight="bold">{item.duration - getDayOf(item.created)}</H1>
-            </MCView>
+            <DividerLine width={120} />
+            <H4 mt={10}>
+              {item.skills.length} {t('outline_skills')}
+            </H4>
           </MCView>
-        </NativeCard>
-      </MCButton>
+          <MCView width={100} align="center">
+            <H4>{t('label_days_left')}</H4>
+            <H1 weight="bold">{item.duration - getDayOf(item.created)}</H1>
+          </MCView>
+        </MCView>
+      </NativeCard>
     );
   };
 
@@ -141,36 +139,37 @@ class MyChallenges extends React.Component {
     const {t, profile, myChallenges, focusedChallenge} = this.props;
     return (
       <MCRootView justify="flex-start" background="transparent">
-        <MCView height={150} justify="center">
-          <FlatList
-            horizontal
-            style={{width: dySize(345)}}
-            contentContainerStyle={{
-              height: dySize(150),
-              alignItems: 'center',
-            }}
-            data={myChallenges}
-            renderItem={this._renderChallengeItem}
-            keyExtractor={item => item._id}
-            ListEmptyComponent={
-              <MCView width={345} align="center">
-                <MCButton
-                  onPress={() => this.onPressAddChallenge()}
-                  bordered
-                  br={10}
-                  pl={40}
-                  pr={40}
-                  pv={10}>
-                  <MCEmptyText>{t('no_challenge')}</MCEmptyText>
-                </MCButton>
-              </MCView>
-            }
-            keyExtractor={item => item._id}
-          />
-        </MCView>
-        <DividerLine width={335} />
         <MCContent
-          contentContainerStyle={{alignItems: 'center', paddingBottom: 50}}>
+          contentContainerStyle={{
+            alignItems: 'center',
+            paddingTop: 20,
+            paddingBottom: 50,
+          }}>
+          {myChallenges.length === 0 ? (
+            <MCView width={345} align="center">
+              <MCButton
+                onPress={() => this.onPressAddChallenge()}
+                bordered
+                br={10}
+                pl={40}
+                pr={40}
+                pv={10}>
+                <MCEmptyText>{t('no_challenge')}</MCEmptyText>
+              </MCButton>
+            </MCView>
+          ) : (
+            <Carousel
+              ref={c => {
+                this._carousel = c;
+              }}
+              data={myChallenges}
+              renderItem={this._renderChallengeItem}
+              sliderWidth={dySize(345)}
+              itemWidth={dySize(250)}
+              onSnapToItem={index => this.onPressChallenge(myChallenges[index])}
+            />
+          )}
+
           {focusedChallenge && (
             <>
               <MCView row mt={20} width={345}>
